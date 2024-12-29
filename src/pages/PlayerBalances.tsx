dde0d6c9b0137c2d9126ad01237fbb727a62df7a
CasualCynic <elliss.jge@gmail.com>
2024-12-28 22:21:52 -0600

    updated /players

--- src/pages/PlayerBalances.tsx
+++ src/pages/PlayerBalances.tsx
@@ -3,6 +3,12 @@ import PlayerBalancesTable from "../components/PlayerBalancesTable";
 function PlayerBalances() {
   return (
     <>
+      <div className="pt-3 ps-3 pe-3 d-flex">
+        <h2 className="display-6">Player Information</h2>
+        <button type="button" className="btn btn-primary ms-auto">
+          Refresh
+        </button>
+      </div>
       <div className="p-3">
         <PlayerBalancesTable />
       </div>
