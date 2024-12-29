21177c3f88e02df2adf87d9682ecd9fa8e855387
CasualCynic <elliss.jge@gmail.com>
2024-12-28 22:49:02 -0600

    updated button

--- src/pages/PlayerBalances.tsx
+++ src/pages/PlayerBalances.tsx
@@ -5,12 +5,20 @@ function PlayerBalances() {
     <>
       <div className="pt-3 ps-3 pe-3 d-flex">
         <h1 className="display-6">Player Information</h1>
-        <button type="button" className="btn btn-primary ms-auto">
+        <button
+          type="button"
+          className="btn btn-primary ms-auto"
+          style={{
+            backgroundColor: "#0d47a1",
+            borderWidth: "0",
+          }}
+        >
           Refresh
         </button>
       </div>
       <div className="p-3">
         <PlayerBalancesTable />
+        {/* <PlayerBalancesTable data={mock or real}/> */}
       </div>
     </>
   );
