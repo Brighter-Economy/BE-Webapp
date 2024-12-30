4f8451aa508f5b7b8bb84f303f0f95fc64c3a13b
CasualCynic <elliss.jge@gmail.com>
2024-12-30 02:03:58 -0600

    update mock player data.

--- src/pages/Auctions.tsx
+++ src/pages/Auctions.tsx
@@ -1,8 +1,16 @@
 function Auctions() {
   return (
     <>
-      <div className="pt-3 ps-3 pe-3 d-flex">
-        <h1 className="display-6">Auctions</h1>
+      <div className="pt-3 ps-3 pe-3 flex-column">
+        <h1 className="display-6 d-block">Auctions</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col bg-primary-subtle rounded m-4">test text</div>
+            <div className="col bg-primary-subtle rounded m-4">test text</div>
+            <div className="col bg-primary-subtle rounded m-4">test text</div>
+            <div className="col bg-primary-subtle rounded m-4">test text</div>
+          </div>
+        </div>
       </div>
     </>
   );
