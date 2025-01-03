809a95bb74c7de6ecf907413dc8bca32708d48cd
CasualCynic <elliss.jge@gmail.com>
2025-01-03 00:08:18 -0600

    Formatted Player Information page
    Tweaked the UX to properly layout

--- src/pages/PlayerBalances.tsx
+++ src/pages/PlayerBalances.tsx
@@ -8,41 +8,48 @@ function PlayerBalances() {
   return (
     <>
       <div className="pt-3 ps-3 pe-3 d-flex">
-        <h1 className="display-6 w-100">Player Information</h1>
-        <div className="input-group input-group me-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="User  Search"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <span className="input-group-text" id="basic-addon2">
-            <i className="bi bi-search" />
-          </span>
+        <div className="container-fluid">
+          <h1 className="display-6 w-100">Player Information</h1>
+          <div className="form-check form-switch me-3 w-100">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="lockedFilter"
+              checked={isLocked}
+              onChange={(e) => setIsLocked(e.target.checked)}
+            />
+            <span>Show Locked Accounts</span>
+          </div>
         </div>
-        <div className="form-check me-3">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="lockedFilter"
-            checked={isLocked}
-            onChange={(e) => setIsLocked(e.target.checked)}
-          />
-          <label className="form-check-label" htmlFor="lockedFilter">
-            Show Locked Accounts
-          </label>
+        <div className="d-flex ms-3 my-auto container-fluid">
+          <div
+            className="input-group input-group ms-auto me-3"
+            style={{ height: 50 }}
+          >
+            <input
+              type="text"
+              className="form-control"
+              placeholder="User Search"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <span className="input-group-text" id="basic-addon2">
+              <i className="bi bi-search" />
+            </span>
+          </div>
+
+          <button
+            type="button"
+            className="btn btn-primary ms-auto"
+            style={{
+              backgroundColor: "#0d47a1",
+              borderWidth: "0",
+              height: 50,
+            }}
+          >
+            Refresh
+          </button>
         </div>
-        <button
-          type="button"
-          className="btn btn-primary ms-auto"
-          style={{
-            backgroundColor: "#0d47a1",
-            borderWidth: "0",
-          }}
-        >
-          Refresh
-        </button>
       </div>
       <div className="p-3">
         <PlayerBalancesTable searchQuery={searchQuery} isLocked={isLocked} />
