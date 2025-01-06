dfbe362ce389a9de124ee2c332d12890bb32807b
Chriss Quartz <chrissquartz@gmail.com>
2025-01-06 15:55:55 +1100

    Add Server Shop filters

--- src/pages/ServerShops.tsx
+++ src/pages/ServerShops.tsx
@@ -2,6 +2,22 @@ import { useState } from "react";
 import { ItemDetails, ShopDetails } from "../components/types";
 import { Link } from "react-router-dom";
 
+function getItemImage(itemName: string) {
+  let itemImg = new Image();
+  itemImg.src =
+    "src/assets/item_images/" + itemName.replace(":", "_") + ".png";
+  return itemImg.src;
+}
+
+function getPrettyItemName(itemID: string) {
+  let itemName = itemID.split(":")[1];
+  itemName = itemName
+    .split("_")
+    .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
+    .join(" ");
+  return itemName;
+}
+
 const ShopCol: React.FC<ShopDetails> = ({
   id,
   ownerUuid,
@@ -9,121 +25,70 @@ const ShopCol: React.FC<ShopDetails> = ({
   item,
   price,
 }) => {
-  function getItemImage(itemName: string) {
-    let itemImg = new Image();
-    itemImg.src =
-      "src/assets/item_images/" + itemName.replace(":", "_") + ".png";
-    return itemImg.src;
-  }
-
-  function getPrettyItemName(itemID: string) {
-    let itemName = itemID.split(":")[1];
-    itemName = itemName
-      .split("_")
-      .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
-      .join(" ");
-    return itemName;
-  }
-
   const [imageSrc, setImageSrc] = useState(getItemImage(item.item));
   const fallbackSrc = "src/assets/no_item_image.png";
   const curPreflix = "$";
 
   return (
-    <>
-      <div
-        className="bg-secondary m-3 shadow rounded-2 flex"
-        style={{ width: 400 }}
-      >
-        <div className="m-3">
-          <div className="d-flex mb-3">
-            <img
-              onError={() => {
-                if (imageSrc !== fallbackSrc) setImageSrc(fallbackSrc);
-              }}
-              src={imageSrc}
-              className="img-fluid rounded me-2 shadow"
-              style={{ width: 80, imageRendering: "pixelated" }}
-            />
-            <div className="m-2 text-start my-auto">
-              <Link to={"/players/" + ownerUuid} style={{ color: "#FFFFFF" }}>
-                <h4>
-                  <strong>{ownerName}'s</strong>
-                </h4>
-              </Link>
-              <h4>{getPrettyItemName(item.item)} Shop</h4>
-            </div>
+    <div className="bg-secondary m-3 shadow rounded-2 flex" style={{ width: 400 }}>
+      <div className="m-3">
+        <div className="d-flex mb-3">
+          <img
+            onError={() => {
+              if (imageSrc !== fallbackSrc) setImageSrc(fallbackSrc);
+            }}
+            src={imageSrc}
+            className="img-fluid rounded me-2 shadow"
+            style={{ width: 80, imageRendering: "pixelated" }}
+          />
+          <div className="m-2 text-start my-auto">
+            <Link to={"/players/" + ownerUuid} style={{ color: "#FFFFFF" }}>
+              <h4>
+                <strong>{ownerName}'s</strong>
+              </h4>
+            </Link>
+            <h4>{getPrettyItemName(item.item)} Shop</h4>
           </div>
-          <div className="d-flex">
-            <div>
-              <div
-                className="input-group mb-1"
-                onClick={() => {
-                  navigator.clipboard.writeText(id);
-                }}
-              >
-                <span className="input-group-text">Shop ID:</span>
-                <input
-                  type="input-group-text"
-                  className="form-control"
-                  disabled={true}
-                  placeholder={id}
-                />
-                <span className="input-group-text">
-                  <i className="bi bi-clipboard2" />
-                </span>
-              </div>
-              <div
-                className="input-group mb-1"
-                onClick={() => {
-                  navigator.clipboard.writeText(
-                    item.count.toString() + "@" + price
-                  );
-                }}
-              >
-                <span className="input-group-text">Price:</span>
-                <input
-                  type="input-group-text"
-                  className="form-control"
-                  disabled={true}
-                  placeholder={
-                    item.count + " @ " + curPreflix + price.toString()
-                  }
-                />
-                <span className="input-group-text">
-                  <i className="bi bi-clipboard2" />
-                </span>
-              </div>
-              <div
-                className="input-group mb-1"
-                onClick={() => {
-                  navigator.clipboard.writeText(item.item);
-                }}
-              >
-                <span className="input-group-text">Item:</span>
-                <input
-                  type="input-group-text"
-                  className="form-control"
-                  disabled={true}
-                  placeholder={item.item}
-                />
-                <span className="input-group-text">
-                  <i className="bi bi-clipboard2" />
-                </span>
-              </div>
+        </div>
+        <div className="d-flex">
+          <div>
+            <div className="input-group mb-1" onClick={() => navigator.clipboard.writeText(id)}>
+              <span className="input-group-text">Shop ID:</span>
+              <input type="input-group-text" className="form-control" disabled={true} placeholder={id} />
+              <span className="input-group-text">
+                <i className="bi bi-clipboard2" />
+              </span>
+            </div>
+            <div className="input-group mb-1" onClick={() => navigator.clipboard.writeText(item.count.toString() + "@" + price)}>
+              <span className="input-group-text">Price:</span>
+              <input type="input-group-text" className="form-control" disabled={true} placeholder={item.count + " @ " + curPreflix + price.toString()} />
+              <span className="input-group-text">
+                <i className="bi bi-clipboard2" />
+              </span>
+            </div>
+            <div className="input-group mb-1" onClick={() => navigator.clipboard.writeText(item.item)}>
+              <span className="input-group-text">Item:</span>
+              <input type="input-group-text" className="form-control" disabled={true} placeholder={item.item} />
+              <span className="input-group-text">
+                <i className="bi bi-clipboard2" />
+              </span>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
 function PlayerShops() {
+  const [playerNameFilter, setPlayerNameFilter] = useState("");
+  const [shopUuidFilter, setShopUuidFilter] = useState("");
+  const [itemNameFilter, setItemNameFilter] = useState("");
+
   const dSword: ItemDetails = {
     item: "minecraft:diamond_sword",
     count: 1,
-    nbt: '{components: {"minecraft:enchantments": {levels: {"minecraft:mending": 1, "minecraft:looting": 3, "minecraft:fire_aspect": 1}}}, count: 1, id: "minecraft:diamond_sword"}',
+    nbt: '{components: {"minecraft:enchantments": {levels: {"minecraft:mending": 1, "minecraft:looting": 3, "minecraft:fire_aspect": 1 }}}, count: 1, id: "minecraft:diamond_sword"}',
   };
 
   const sPlank: ItemDetails = {
@@ -162,6 +127,16 @@ function PlayerShops() {
     price: 20,
   };
 
+  const shops = [shop1, shop2, shop3];
+
+  const filteredShops = shops.filter((shop) => {
+    const matchesPlayerName = shop.ownerName.toLowerCase().startsWith(playerNameFilter.toLowerCase());
+    const matchesShopUuid = shop.id.startsWith(shopUuidFilter);
+    const matchesItemName = getPrettyItemName(shop.item.item).toLowerCase().startsWith(itemNameFilter.toLowerCase());
+
+    return matchesPlayerName && matchesShopUuid && matchesItemName;
+  });
+
   return (
     <>
       <div className="d-flex">
@@ -169,27 +144,33 @@ function PlayerShops() {
           <h1 className="display-6">Player Shops</h1>
         </div>
         <div className="d-flex ms-3 my-auto container-fluid">
-          <div
-            className="input-group input-group ms-auto me-3"
-            style={{ height: 50 }}
-          >
-            <input
-              type="text"
-              className="form-control"
-              placeholder="User or Item Search..."
-            />
-            <span className="input-group-text" id="basic-addon2">
-              <i className="bi bi-search" />
-            </span>
-          </div>
-
+          <input
+            type="text"
+            className="form-control me-2"
+            placeholder="Player Name"
+            value={playerNameFilter}
+            onChange={(e) => setPlayerNameFilter(e.target.value)}
+          />
+          <input
+            type="text"
+            className="form-control me-2"
+            placeholder="Shop UUID"
+            value={shopUuidFilter}
+            onChange={(e) => setShopUuidFilter(e.target.value)}
+          />
+          <input
+            type="text"
+            className="form-control me-2"
+            placeholder="Item Name"
+            value={itemNameFilter}
+            onChange={(e) => setItemNameFilter(e.target.value)}
+          />
           <button
             type="button"
-            className="btn btn-primary ms-auto"
+            className="btn btn-primary"
             style={{
               backgroundColor: "#0d47a1",
               borderWidth: "0",
-              height: 50,
             }}
           >
             Refresh
@@ -198,27 +179,16 @@ function PlayerShops() {
       </div>
       <div className="container w-100 d-flex">
         <div className="row mx-auto g-0">
-          <ShopCol
-            id={shop1.id}
-            ownerUuid={shop1.ownerUuid}
-            ownerName={shop1.ownerName}
-            item={shop1.item}
-            price={shop1.price}
-          />
-          <ShopCol
-            id={shop2.id}
-            ownerUuid={shop2.ownerUuid}
-            ownerName={shop2.ownerName}
-            item={shop2.item}
-            price={shop2.price}
-          />
-          <ShopCol
-            id={shop3.id}
-            ownerUuid={shop3.ownerUuid}
-            ownerName={shop3.ownerName}
-            item={shop3.item}
-            price={shop3.price}
-          />
+          {filteredShops.map((shop) => (
+            <ShopCol
+              key={shop.id}
+              id={shop.id}
+              ownerUuid={shop.ownerUuid}
+              ownerName={shop.ownerName}
+              item={shop.item}
+              price={shop.price}
+            />
+          ))}
         </div>
       </div>
     </>
