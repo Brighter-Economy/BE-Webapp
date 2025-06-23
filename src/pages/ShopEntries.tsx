ed8b18a3b41e1e6d5f2e9008255f24a1b1d96c5d
Chriss Quartz <chrissquartz@gmail.com>
2025-06-24 09:41:56 +1000

    Shop entries now actually show the correct item.

--- src/pages/ShopEntries.tsx
+++ src/pages/ShopEntries.tsx
@@ -9,7 +9,6 @@ function ShopEntries() {
     const fallbackSrc = "src/assets/no_item_image.png";
 
     const [shopData, setShopData] = useState<ShopDetails[]>([]);
-    const [imageSrc, setImageSrc] = useState<string>(fallbackSrc);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
@@ -37,14 +36,6 @@ function ShopEntries() {
         fetchData();
     }, [pathname]);
 
-    useEffect(() => {
-        if (shopData) {
-            shopData.map((item) => {
-                setImageSrc(getItemImage(item.itemStack.item));
-            })
-        }
-    }, [shopData]);
-
     if (loading) {
         return <div>Loading...</div>;
     }
@@ -53,43 +44,45 @@ function ShopEntries() {
         return <div>Error: {error}</div>;
     }
 
-    const glintEffect = () => {
-        return shopData.map((item) => {
-            if (item.itemStack.enchantments.length > 0) {
-                return (
-                    <div className="item-mask" style={{ maskImage: `url("${imageSrc}")` }} key={item.id}>
-                        <img src={glintImage} className="glint" />
-                    </div>
-                );
-            }
-
-            return null;
-        });
+    const glintEffect = (item: ShopDetails) => {
+        if (item.itemStack.enchantments.length > 0) {
+            return (
+                <div className="item-mask" style={{ maskImage: `url("${getItemImage(item.itemStack.item)}")` }} key={item.id}>
+                    <img src={glintImage} className="glint" />
+                </div>
+            );
+        }
+        return null;
     }
 
     return (
         <>
             <div className="pt-3 ps-3 pe-3 d-flex flex-column">
                 {shopData.map((item) => {
-                    return <div className="shop-entry mb-3" key={item.id}>
-                        <h2>{item.ownerName}'s Shop</h2>
-                        <p>Owner UUID: {item.ownerUuid}</p>
-                        <p>Dimension: {item.dimension}</p>
-                        <p>Position: {`(${item.position.x}, ${item.position.y}, ${item.position.z})`}</p>
-                        <p>Item Stack: {JSON.stringify(item.itemStack)}</p>
-                        <p>Price: ${item.price.toFixed(2)}</p>
+                    const imageSrc = getItemImage(item.itemStack.item);
 
-                        <ItemToolTip itemStack={item.itemStack}>
-                            <img
-                                onError={() =>
-                                    imageSrc !== fallbackSrc && setImageSrc(fallbackSrc)
-                                }
-                                src={imageSrc}
-                                className="me-2 standard-item"
-                            />
-                            {glintEffect()}
-                        </ItemToolTip>
-                    </div>
+                    return (
+                        <div className="shop-entry mb-3" key={item.id}>
+                            <h2>{item.ownerName}'s Shop</h2>
+                            <p>Owner UUID: {item.ownerUuid}</p>
+                            <p>Dimension: {item.dimension}</p>
+                            <p>Position: {`(${item.position.x}, ${item.position.y}, ${item.position.z})`}</p>
+                            <p>Item Stack: {JSON.stringify(item.itemStack)}</p>
+                            <p>Price: ${item.price.toFixed(2)}</p>
+
+                            <ItemToolTip itemStack={item.itemStack}>
+                                <img
+                                    onError={(e) => {
+                                        const target = e.target as HTMLImageElement;
+                                        target.src = fallbackSrc;
+                                    }}
+                                    src={imageSrc}
+                                    className="me-2 standard-item"
+                                />
+                                {glintEffect(item)}
+                            </ItemToolTip>
+                        </div>
+                    );
                 })}
             </div>
         </>
