8cdfba76592cbb874cc3db06c65e5d4dd2b9e126
Chriss Quartz <chrissquartz@gmail.com>
2025-06-23 13:11:43 +1000

    Fixed doubled up code

--- src/components/ShopEntry.tsx
+++ src/components/ShopEntry.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { ItemStack, ShopDetails } from "../components/types";
-import { ItemStack, ShopDetails } from "../components/types";
 import { Link } from "react-router-dom";
 import ItemToolTip from "./ItemToolTip";
 import "./ShopEntry.css";
@@ -15,132 +14,115 @@ const ShopEntry: React.FC<ShopEntryParameters> = ({ shopDetails }) => {
   const [imageSrc, setImageSrc] = useState(
     getItemImage(shopDetails.itemStack.item)
   );
-  const ShopEntry: React.FC<ShopEntryParameters> = ({ shopDetails }) => {
-    const [imageSrc, setImageSrc] = useState(
-      getItemImage(shopDetails.itemStack.item)
+  const fallbackSrc = "src/assets/no_item_image.png";
+  const currencyPreflix = "$";
+
+  const glintEffect = () =>
+    shopDetails.itemStack.enchantments.length > 0 && (
+      <div className="item-mask" style={{ maskImage: `url("${imageSrc}")` }}>
+        <img src={glintImage} className="glint" />
+      </div>
     );
-    const fallbackSrc = "src/assets/no_item_image.png";
-    const currencyPreflix = "$";
 
-    const glintEffect = () =>
-      shopDetails.itemStack.enchantments.length > 0 && (
-        <div className="item-mask" style={{ maskImage: `url("${imageSrc}")` }}>
-          <img src={glintImage} className="glint" />
-        </div>
-      );
-
-    return (
-      <div
-        className="bg-secondary m-3 shadow rounded-2 flex"
-        style={{ width: 400 }}
-      >
-        <div className="m-3">
-          <div className="d-flex mb-3">
-            <ItemToolTip itemStack={shopDetails.itemStack}>
-              <img
-                onError={() =>
-                  imageSrc !== fallbackSrc && setImageSrc(fallbackSrc)
-                }
-                src={imageSrc}
-                className="me-2 standard-item"
-              />
-              {glintEffect()}
-            </ItemToolTip>
-            <div className="m-2 text-start my-auto">
-              <Link
-                to={"/players/" + shopDetails.ownerUuid}
-                style={{ color: "#FFFFFF" }}
-              >
-                <Link
-                  to={"/players/" + shopDetails.ownerUuid}
-                  style={{ color: "#FFFFFF" }}
-                >
-                  <h4>
-                    <strong>{shopDetails.ownerName}'s</strong>
-                    <strong>{shopDetails.ownerName}'s</strong>
-                  </h4>
-                </Link>
-                <h4>{getPrettyItemName(shopDetails.itemStack)} Shop</h4>
-                <h4>{getPrettyItemName(shopDetails.itemStack)} Shop</h4>
-            </div>
+  return (
+    <div
+      className="bg-secondary m-3 shadow rounded-2 flex"
+      style={{ width: 400 }}
+    >
+      <div className="m-3">
+        <div className="d-flex mb-3">
+          <ItemToolTip itemStack={shopDetails.itemStack}>
+            <img
+              onError={() =>
+                imageSrc !== fallbackSrc && setImageSrc(fallbackSrc)
+              }
+              src={imageSrc}
+              className="me-2 standard-item"
+            />
+            {glintEffect()}
+          </ItemToolTip>
+          <div className="m-2 text-start my-auto">
+            <Link
+              to={"/players/" + shopDetails.ownerUuid}
+              style={{ color: "#FFFFFF" }}
+            >
+              <h4>
+                <strong>{shopDetails.ownerName}'s</strong>
+              </h4>
+            </Link>
+            <h4>{getPrettyItemName(shopDetails.itemStack)} Shop</h4>
           </div>
-          <div className="d-flex">
-            <div>
-              <div
-                className="input-group mb-1"
-                onClick={() => navigator.clipboard.writeText(shopDetails.id)}
-                onClick={() => navigator.clipboard.writeText(shopDetails.id)}
-              >
-                <span className="input-group-text">Shop ID:</span>
-                <input
-                  type="input-group-text"
-                  className="form-control"
-                  disabled={true}
-                  placeholder={shopDetails.id}
-                  placeholder={shopDetails.id}
-                />
-                <span className="input-group-text">
-                  <i className="bi bi-clipboard2" />
-                </span>
-              </div>
-              <div
-                className="input-group mb-1"
-                onClick={() =>
-                  navigator.clipboard.writeText(
-                    shopDetails.itemStack.count.toString() +
-                    "@" +
-                    shopDetails.price
-                  )
+        </div>
+        <div className="d-flex">
+          <div>
+            <div
+              className="input-group mb-1"
+              onClick={() => navigator.clipboard.writeText(shopDetails.id)}
+
+            >
+              <span className="input-group-text">Shop ID:</span>
+              <input
+                type="input-group-text"
+                className="form-control"
+                disabled={true}
+                placeholder={shopDetails.id}
+
+              />
+              <span className="input-group-text">
+                <i className="bi bi-clipboard2" />
+              </span>
+            </div>
+            <div
+              className="input-group mb-1"
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  shopDetails.itemStack.count.toString() +
+                  "@" +
+                  shopDetails.price
+                )
+              }
+            >
+              <span className="input-group-text">Price:</span>
+              <input
+                type="input-group-text"
+                className="form-control"
+                disabled={true}
+                placeholder={
+                  shopDetails.itemStack.count +
+                  " @ " +
+                  currencyPreflix +
+                  shopDetails.price.toString()
                 }
-              >
-                <span className="input-group-text">Price:</span>
-                <input
-                  type="input-group-text"
-                  className="form-control"
-                  disabled={true}
-                  placeholder={
-                    shopDetails.itemStack.count +
-                    " @ " +
-                    currencyPreflix +
-                    shopDetails.price.toString()
-                  }
-                  placeholder={
-                    shopDetails.itemStack.count +
-                    " @ " +
-                    currencyPreflix +
-                    shopDetails.price.toString()
-                  }
-                />
-                <span className="input-group-text">
-                  <i className="bi bi-clipboard2" />
-                </span>
-              </div>
-              <div
-                className="input-group mb-1"
-                onClick={() =>
-                  navigator.clipboard.writeText(shopDetails.itemStack.item)
-                }
-                onClick={() =>
-                  navigator.clipboard.writeText(shopDetails.itemStack.item)
-                }
-              >
-                <span className="input-group-text">Item:</span>
-                <input
-                  type="input-group-text"
-                  className="form-control"
-                  disabled={true}
-                  placeholder={shopDetails.itemStack.item}
-                  placeholder={shopDetails.itemStack.item}
-                />
-                <span className="input-group-text">
-                  <i className="bi bi-clipboard2" />
-                </span>
-              </div>
+
+              />
+              <span className="input-group-text">
+                <i className="bi bi-clipboard2" />
+              </span>
+            </div>
+            <div
+              className="input-group mb-1"
+              onClick={() =>
+                navigator.clipboard.writeText(shopDetails.itemStack.item)
+              }
+
+            >
+              <span className="input-group-text">Item:</span>
+              <input
+                type="input-group-text"
+                className="form-control"
+                disabled={true}
+                placeholder={shopDetails.itemStack.item}
+
+              />
+              <span className="input-group-text">
+                <i className="bi bi-clipboard2" />
+              </span>
             </div>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default ShopEntry;
+export default ShopEntry;
